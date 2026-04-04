0a31cd6aab6c568beb661de3a9a2f8402777c622
옥물결 <okmoolgyul@ogmulgyeol-ui-MacBookPro.local>
2026-04-04 11:27:55 -0700

    frist

--- backjoon/backjoonApp.swift
+++ backjoon/backjoonApp.swift
@@ -1,17 +1,2 @@
-//
-//  backjoonApp.swift
-//  backjoon
-//
-//  Created by 옥물결 on 4/4/26.
-//
-
-import SwiftUI
-
-@main
-struct backjoonApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+// 1000번
+let input = readLine()!.split(separa)
