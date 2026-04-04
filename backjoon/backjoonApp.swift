f95642e9f41cb7546b0bf3d98ece8d76c7c8aa72
옥물결 <okmoolgyul@ogmulgyeol-ui-MacBookPro.local>
2026-04-04 11:21:44 -0700

    test

---
+++ backjoon/backjoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  backjoonApp.swift
+//  backjoon
+//
+//  Created by 옥물결 on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct backjoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
